7683e441dcec9112a57141c84f0881e2202da252
James Talton <jtalton@redhat.com>
2020-10-04 15:30:25 -0400

    Switch testing to @testing-library/react

--- src/ExampleJSComponent/ExampleJSComponent.test.jsx
+++ src/ExampleJSComponent/ExampleJSComponent.test.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { render } from '@testing-library/react'
 import ExampleJSComponent from './ExampleJSComponent'
 
 describe('ExampleJSComponent', () => {
     test('Render a h1 element', () => {
-        const wrapper = shallow(<ExampleJSComponent text="Test Header" />)
-        expect(wrapper.find('h1').length).toBe(1)
-        expect(wrapper.find('h1').contains('Test Header'))
+        const { getByText } = render(<ExampleJSComponent text="Test Header" />)
+        expect(getByText('Test Header')).toBeInTheDocument()
     })
 })
