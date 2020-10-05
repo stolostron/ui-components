2fff7d82c8c6d3d193fd79d1580978221687d0ab
Sho Weimer <sweimer@redhat.com>
2020-10-05 12:23:38 -0400

    revert path wildcarding

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 const sass = require('sass');
 
 module.exports = {
-    stories: ['../src/**/**/*.stories.tsx', '../src/**/**/*.stories.jsx'],
+    stories: ['../src/**/*.stories.tsx', '../src/**/*.stories.jsx'],
     webpackFinal: async config => {
         config.module.rules.push(
             {
