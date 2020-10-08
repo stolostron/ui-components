19bf9880d60e4e60caa0464c6ed73d4110e7e0ad
Sho Weimer <sweimer@redhat.com>
2020-10-08 19:01:55 -0400

    fix wrong test name

--- src/AcmLabels/AcmLabels.test.tsx
+++ src/AcmLabels/AcmLabels.test.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import { AcmLabels } from './AcmLabels'
 
-describe('AcmForm', () => {
+describe('AcmLabels', () => {
     test('renders', () => {
         const { getByText } = render(<AcmLabels labels={['foo=bar', 'cluster=management']} />)
         expect(getByText('foo=bar')).toBeInTheDocument()
