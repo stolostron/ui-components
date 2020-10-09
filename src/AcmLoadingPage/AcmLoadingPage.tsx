5676367ad5f3ecc87c757e95f0dd65e2d1673e47
James Talton <jtalton@redhat.com>
2020-10-09 16:52:34 -0400

    Update loading page

--- src/AcmLoadingPage/AcmLoadingPage.tsx
+++ src/AcmLoadingPage/AcmLoadingPage.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { EmptyState, EmptyStateIcon, Title, Spinner } from '@patternfly/react-core'
+import { AcmPageCard } from '../AcmPage/AcmPage'
 
 export function AcmLoadingPage() {
     return (
-        <EmptyState>
-            <EmptyStateIcon variant="container" component={Spinner} />
-            <Title size="lg" headingLevel="h4">
-                Loading
-            </Title>
-        </EmptyState>
+        <AcmPageCard>
+            <EmptyState>
+                <EmptyStateIcon variant="container" component={Spinner} />
+                <Title size="lg" headingLevel="h4">
+                    Loading
+                </Title>
+            </EmptyState>
+        </AcmPageCard>
     )
 }
