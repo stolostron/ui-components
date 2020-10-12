c1c6d6690f1930583f5e61dd7eae8afd95a35f18
randybrunopiverger <rbrunopi@redhat.com>
2020-10-12 13:26:20 -0400

    AcmSecondaryNav

---
+++ src/AcmSecondaryNav/AcmSecondaryNav.stories.tsx
@@ -0,0 +1,33 @@
+import '@patternfly/react-core/dist/styles/base.css'
+import React, { useEffect, useState } from 'react'
+import { Meta } from '@storybook/react'
+import { AcmSecondaryNav, AcmSecondaryNavItem } from './AcmSecondaryNav'
+import { AcmPage, AcmPageCard, AcmPageHeader } from '../AcmPage/AcmPage'
+
+const meta: Meta = {
+    title: 'SecondaryNav',
+    component: AcmSecondaryNav,
+}
+export default meta
+
+export function SecondaryNav() {
+    const [active, setActive] = useState('first')
+    return (
+        <AcmPage>
+            <AcmPageHeader title="SecondaryNav"></AcmPageHeader>
+            <AcmPageCard>
+                <AcmSecondaryNav>
+                    <AcmSecondaryNavItem isActive={active === 'first'} onClick={() => setActive('first')}>
+                        First Nav
+                    </AcmSecondaryNavItem>
+                    <AcmSecondaryNavItem isActive={active === 'second'} onClick={() => setActive('second')}>
+                        Second Nav
+                    </AcmSecondaryNavItem>
+                    <AcmSecondaryNavItem isActive={active === 'third'} onClick={() => setActive('third')}>
+                        Third Nav
+                    </AcmSecondaryNavItem>
+                </AcmSecondaryNav>
+            </AcmPageCard>
+        </AcmPage>
+    )
+}
---
+++ src/AcmSecondaryNav/AcmSecondaryNav.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { AcmSecondaryNav, AcmSecondaryNavItem } from './AcmSecondaryNav'
+
+describe('AcmSecondaryNav', () => {
+    test('renders', () => {
+        const { getByText } = render(
+            <AcmSecondaryNav>
+                <AcmSecondaryNavItem isActive={false} to={'/multicloud/test'}> Tab1 </AcmSecondaryNavItem>
+            </AcmSecondaryNav>
+        )
+
+        expect(getByText('Tab1')).toBeInTheDocument()
+        expect(getByText('Tab1')).toBeInstanceOf(HTMLAnchorElement)
+        expect(getByText('Tab1').getAttribute('href')).toEqual('/multicloud/test')  
+    })
+})
