d1ad6ab5dbc107c71bcb083d612b385e0913ec14
Randy Bruno Piverger <randybruno424@gmail.com>
2020-10-14 10:10:08 -0400

    AcmSecondaryNav (#8)
    * AcmSecondaryNav
    
    * formatting

---
+++ src/AcmSecondaryNav/AcmSecondaryNav.tsx
@@ -0,0 +1,24 @@
+import { Nav, NavItem, NavList, PageSection, PageSectionVariants } from '@patternfly/react-core'
+import React, { ReactNode } from 'react'
+
+export function AcmSecondaryNav(props: { children: ReactNode }) {
+    return (
+        <PageSection variant={PageSectionVariants.light} padding={{ default: 'noPadding' }}>
+            <Nav variant="tertiary" style={{ paddingLeft: '12px' }}>
+                <NavList>{props.children}</NavList>
+            </Nav>
+        </PageSection>
+    )
+}
+export function AcmSecondaryNavItem(props: {
+    onClick?: () => void
+    isActive: boolean
+    to?: string
+    children: ReactNode
+}) {
+    return (
+        <NavItem onClick={props.onClick} isActive={props.isActive} to={props.to}>
+            {props.children}
+        </NavItem>
+    )
+}
