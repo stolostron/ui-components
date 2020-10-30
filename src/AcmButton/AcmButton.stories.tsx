0d70ac81275da2aea41ca840a30be1102f340e04
Sho Weimer <sweimer@redhat.com>
2020-10-29 21:14:50 -0400

    add a few more button control enhancements

--- src/AcmButton/AcmButton.stories.tsx
+++ src/AcmButton/AcmButton.stories.tsx
@@ -2,14 +2,20 @@ import '@patternfly/react-core/dist/styles/base.css'
 import React from 'react'
 import { Meta } from '@storybook/react'
 import { AcmButton } from './AcmButton'
+import { ButtonVariant } from '@patternfly/react-core'
 
 const meta: Meta = {
     title: 'Button',
     component: AcmButton,
     argTypes: {
         isDisabled: { control: 'boolean' },
+        text: { type: 'string' },
+        variant: {
+            control: { type: 'select', options: Object.values(ButtonVariant) },
+        },
     },
 }
 export default meta
 
-export const Button = (args) => <AcmButton {...args}>Button</AcmButton>
+export const Button = (args) => <AcmButton {...args}>{args.text}</AcmButton>
+Button.args = { text: 'Button' }
