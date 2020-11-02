ff4ba55cf906ed56574d208f947976fdcac544aa
James Talton <jtalton@redhat.com>
2020-11-02 16:15:41 -0500

    labels support object

--- src/AcmLabels/AcmLabels.tsx
+++ src/AcmLabels/AcmLabels.tsx
@@ -2,14 +2,29 @@ import { Label } from '@patternfly/react-core'
 import '@patternfly/react-core/dist/styles/base.css'
 import React, { Fragment } from 'react'
 
-export function AcmLabels(props: { labels: string[] }) {
-    return (
-        <Fragment>
-            {props.labels.map((label) => (
-                <Label key={label} style={{ margin: 1 }}>
-                    {label}
-                </Label>
-            ))}
-        </Fragment>
-    )
+export function AcmLabels(props: { labels: string[] | Record<string, string> | undefined }) {
+    const { labels } = props
+    if (labels === undefined) {
+        return <Fragment />
+    } else if (Array.isArray(labels)) {
+        return (
+            <Fragment>
+                {labels.map((label) => (
+                    <Label key={label} style={{ margin: 1 }}>
+                        {label}
+                    </Label>
+                ))}
+            </Fragment>
+        )
+    } else {
+        return (
+            <Fragment>
+                {Object.keys(labels).map((key) => (
+                    <Label key={key} style={{ margin: 1 }}>
+                        {key}={labels[key]}
+                    </Label>
+                ))}
+            </Fragment>
+        )
+    }
 }
