5ff9d8c6fcc7ced98fa0793147bfcb4e354baeb0
James Talton <jtalton@redhat.com>
2020-11-02 17:25:24 -0500

    labels support object (#33)
    * labels support object
    
    * linting

--- src/AcmLabels/AcmLabels.test.tsx
+++ src/AcmLabels/AcmLabels.test.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
 import { render } from '@testing-library/react'
 import { axe } from 'jest-axe'
+import React from 'react'
 import { AcmLabels } from './AcmLabels'
 
 describe('AcmLabels', () => {
-    test('renders', () => {
+    test('renders empty with undefined', () => {
+        const { container } = render(<AcmLabels labels={undefined} />)
+        expect(container).toMatchInlineSnapshot('<div />')
+    })
+    test('renders with string array', () => {
         const { getByText } = render(<AcmLabels labels={['foo=bar', 'cluster=management']} />)
+        expect(getByText('foo=bar')).toBeInTheDocument()
+        expect(getByText('cluster=management')).toBeInstanceOf(HTMLSpanElement)
+    })
+    test('renders with object', () => {
+        const { getByText } = render(<AcmLabels labels={{ foo: 'bar', cluster: 'management' }} />)
         expect(getByText('foo=bar')).toBeInTheDocument()
         expect(getByText('cluster=management')).toBeInstanceOf(HTMLSpanElement)
     })
