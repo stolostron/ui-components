0fc0d161a0c6e3376cea58791614c47150627f97
Sho Weimer <sweimer@redhat.com>
2020-11-04 15:53:45 -0500

    redo modal

--- src/AcmButton/AcmButton.tsx
+++ src/AcmButton/AcmButton.tsx
@@ -1,7 +1,12 @@
-import { Button } from '@patternfly/react-core'
+import { Button, ButtonVariant } from '@patternfly/react-core'
 import React, { ReactNode } from 'react'
 
-export function AcmButton(props: { children: ReactNode; isDisabled?: boolean; onClick: () => void }) {
+export function AcmButton(props: {
+    children: ReactNode
+    isDisabled?: boolean
+    onClick: () => void
+    variant?: ButtonVariant
+}) {
     return (
         <Button {...props} isDisabled={props.isDisabled}>
             {props.children}
--- src/AcmModal/AcmModal.stories.tsx
+++ src/AcmModal/AcmModal.stories.tsx
@@ -1,27 +1,53 @@
 import '@patternfly/react-core/dist/styles/base.css'
 import React, { useState } from 'react'
 import { Meta } from '@storybook/react'
+import { ButtonVariant, ModalVariant } from '@patternfly/react-core'
+import { AcmPageCard } from '../AcmPage/AcmPage'
 import { AcmModal } from './AcmModal'
 import { AcmButton } from '../'
 
 const meta: Meta = {
     title: 'Modal',
     component: AcmModal,
+    argTypes: {
+        title: { type: 'string' },
+        message: { type: 'string' },
+        variant: {
+            control: { type: 'select', options: Object.values(ModalVariant) },
+        },
+        titleIconVariant: {
+            control: { type: 'select', options: ['success', 'danger', 'warning', 'info', 'default'] },
+        },
+    },
 }
 export default meta
 
-export const Modal = () => {
+export const Modal = (args) => {
     const [open, toggleOpen] = useState<boolean>(true)
+    const toggle = () => toggleOpen(!open)
     return (
-        <React.Fragment>
-            <AcmButton onClick={() => toggleOpen(true)}>Open Modal</AcmButton>
+        <AcmPageCard>
+            <AcmButton onClick={toggle}>Open Modal</AcmButton>
             <AcmModal
-                open={open}
-                submit={() => toggleOpen((prevOpen) => !prevOpen)}
-                cancel={() => toggleOpen((prevOpen) => !prevOpen)}
-                title="ACM Modal"
-                message="Modal message here"
-            />
-        </React.Fragment>
+                variant={args.variant}
+                titleIconVariant={args.titleIconVariant}
+                isOpen={open}
+                title={args.title}
+                actions={[
+                    <AcmButton key="confirm" variant={ButtonVariant.primary} onClick={toggle}>
+                        Submit
+                    </AcmButton>,
+                    <AcmButton key="cancel" variant={ButtonVariant.link} onClick={toggle}>
+                        Cancel
+                    </AcmButton>,
+                ]}
+            >
+                {args.message}
+            </AcmModal>
+        </AcmPageCard>
     )
+}
+Modal.args = {
+    title: 'ACM Modal',
+    message: 'Modal message here',
 }
