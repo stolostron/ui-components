19483360f813adb7fcd35e20546400c67a61d423
James Talton <jtalton@redhat.com>
2020-11-05 13:38:29 -0500

    table hide checkboxes and allow column tooltip (#36)

--- src/AcmButton/AcmButton.tsx
+++ src/AcmButton/AcmButton.tsx
@@ -1,15 +1,6 @@
-import { Button, ButtonVariant } from '@patternfly/react-core'
-import React, { ReactNode } from 'react'
+import { Button, ButtonProps } from '@patternfly/react-core'
+import React from 'react'
 
-export function AcmButton(props: {
-    children: ReactNode
-    isDisabled?: boolean
-    onClick: () => void
-    variant?: ButtonVariant
-}) {
-    return (
-        <Button {...props} isDisabled={props.isDisabled}>
-            {props.children}
-        </Button>
-    )
+export function AcmButton(props: ButtonProps) {
+    return <Button {...props} />
 }
