76d544c2eecda3e2ffe047ac700223bc4e0aeaba
James Talton <jtalton@redhat.com>
2020-11-16 16:01:51 -0500

    fix test

--- src/AcmTableLoading/AcmTableLoading.test.tsx
+++ src/AcmTableLoading/AcmTableLoading.test.tsx
@@ -1,10 +1,10 @@
 import { render } from '@testing-library/react'
 import React from 'react'
-import { TableLoading } from './AcmTableLoading.stories'
+import { AcmTableLoading } from './AcmTableLoading'
 
 describe('AcmTableLoading', () => {
     test('renders', () => {
-        const { container } = render(<TableLoading />)
+        const { container } = render(<AcmTableLoading />)
         expect(container.querySelector('table')).toBeInTheDocument()
     })
 })
