730fa9f0a8b1fabcae43881e5e3bd9fda5c524fe
Zack Layne <zlayne@redhat.com>
2020-11-18 15:07:28 -0500

    Add Tile component (#54)
    * Add Tile component
    
    * use pf color var
    
    * skeleton component & review edits
    
    * fix tests
    
    * fix wrap

---
+++ src/AcmTile/AcmTile.stories.tsx
@@ -0,0 +1,51 @@
+import '@patternfly/react-core/dist/styles/base.css'
+import React, { useState } from 'react'
+import { Meta } from '@storybook/react'
+import { AcmTile } from './AcmTile'
+
+const meta: Meta = {
+    title: 'Tile',
+    component: AcmTile,
+    argTypes: {
+        title: { type: 'string' },
+        message: { type: 'string' },
+    },
+}
+export default meta
+
+export const Tile = () => {
+    const [selected, toggleSelected] = useState<boolean>(false)
+
+    return (
+        <div style={{ display: 'flex' }}>
+            <AcmTile
+                loading={false}
+                isSelected={selected}
+                title={''}
+                onClick={() => toggleSelected(!selected)}
+                relatedResourceData={{ count: 99999, kind: 'veryLongKindNameForTestingPurposes' }}
+            />
+            <AcmTile
+                loading={false}
+                isSelected={selected}
+                title={''}
+                onClick={() => toggleSelected(!selected)}
+                relatedResourceData={{ count: 1, kind: 'veryShort' }}
+            />
+        </div>
+    )
+}
+
+export const LoadingTile = () => {
+    const [selected, toggleSelected] = useState<boolean>(false)
+
+    return (
+        <AcmTile
+            loading={true}
+            isSelected={selected}
+            title={'testing'}
+            onClick={() => toggleSelected(!selected)}
+            relatedResourceData={{ count: 1, kind: 'veryLongKindNameForTestingPurposes' }}
+        />
+    )
+}
