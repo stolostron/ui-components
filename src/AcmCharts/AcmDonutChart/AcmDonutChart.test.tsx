7a8370edebcbb22c0689d99a7f0ac5abdaf86cc6
Sho Weimer <sweimer@redhat.com>
2020-11-23 15:59:29 -0500

    donut chart tests

--- src/AcmCharts/AcmDonutChart/AcmDonutChart.test.tsx
+++ src/AcmCharts/AcmDonutChart/AcmDonutChart.test.tsx
@@ -1,13 +1,9 @@
 import React from 'react'
+import { render } from '@testing-library/react'
+import { axe } from 'jest-axe'
 import { AcmDonutChart } from './AcmDonutChart'
-import { AcmChartGroup } from '../AcmChartGroup'
 
-export default {
-    title: 'AcmChart',
-    component: AcmDonutChart,
-}
-
-export const DonutChart = () => {
+describe('AcmDonutChart', () => {
     const complianceData = [
         { key: 'Compliant', value: 1, isPrimary: true },
         { key: 'Non-compliant', value: 1, isDanger: true },
@@ -17,19 +13,24 @@ export const DonutChart = () => {
         { key: 'Pending', value: 8 },
         { key: 'Failed', value: 2, isDanger: true },
     ]
-    const clusterData = [
-        { key: 'Ready', value: 2, isPrimary: true },
-        { key: 'Offline', value: 0, isDanger: true },
-    ]
-    return (
-        <AcmChartGroup>
+    // const clusterData = [
+    //     { key: 'Ready', value: 2, isPrimary: true },
+    //     { key: 'Offline', value: 0, isDanger: true },
+    // ]
+    test('renders', () => {
+        const { getByTestId } = render(
             <AcmDonutChart
                 title="Cluster compliance"
                 description="Overview of policy compliance status"
                 data={complianceData}
             />
+        )
+        expect(getByTestId('cluster-compliance-chart')).toBeInTheDocument()
+    })
+    test('has zero accessibility defects', async () => {
+        const { container } = render(
             <AcmDonutChart title="Pods" description="Overview of pod count and status" data={podData} />
-            <AcmDonutChart title="Cluster status" description="Overview of cluster status" data={clusterData} />
-        </AcmChartGroup>
-    )
-}
+        )
+        expect(await axe(container)).toHaveNoViolations()
+    })
+})
--- src/AcmCharts/AcmDonutChart/AcmDonutChart.tsx
+++ src/AcmCharts/AcmDonutChart/AcmDonutChart.tsx
@@ -1,11 +1,71 @@
 import React from 'react'
-import { Card } from '@patternfly/react-core'
+import { Card, CardTitle, Badge } from '@patternfly/react-core'
 import { ChartDonut } from '@patternfly/react-charts'
+import { makeStyles } from '@material-ui/styles'
+import { useViewport } from '../AcmChartGroup'
 
-export function AcmDonutChart(props) {
+type StyleProps = {
+    danger?: boolean
+    viewWidth: number
+}
+
+/* istanbul ignore next */
+const useStyles = makeStyles({
+    card: {
+        maxHeight: '259px',
+        minWidth: (props: StyleProps) => (props.viewWidth > 376 ? '376px' : ''),
+        maxWidth: (props: StyleProps) => (props.viewWidth < 376 ? '376px' : ''),
+        '& .pf-c-chart svg g path:last-of-type': {
+            fill: (props: StyleProps) => (props.danger ? '#E62325 !important' : undefined),
+        },
+    },
+    cardTitle: {
+        paddingBottom: 'unset !important',
+    },
+    chartContainer: {
+        maxWidth: '376px',
+    },
+})
+
+export function AcmDonutChart(props: {
+    title: string
+    description: string
+    data: { key: string; value: number; isPrimary?: boolean; isDanger?: boolean }[]
+}) {
+    const chartData = props.data.map((d) => ({ x: d.key, y: d.value }))
+    const legendData = props.data.map((d) => ({ name: `${d.value} ${d.key}` }))
+    const total = props.data.reduce((a, b) => a + b.value, 0)
+    const primary = props.data.find((d) => d.isPrimary)
+
+    const { viewWidth } = useViewport()
+    const classes = useStyles({ danger: props.data.some((d) => d.isDanger), viewWidth } as StyleProps)
     return (
-        <Card>
-            Hello
+        <Card className={classes.card} id={`${props.title.toLowerCase().replace(/\s+/g, '-')}-chart`}>
+            <CardTitle className={classes.cardTitle}>
+                {props.title} <Badge isRead>{total}</Badge>
+            </CardTitle>
+            <div className={classes.chartContainer}>
+                <ChartDonut
+                    ariaTitle={props.title}
+                    ariaDesc={props.description}
+                    legendOrientation="vertical"
+                    legendPosition="right"
+                    constrainToVisibleArea={true}
+                    data={chartData}
+                    legendData={legendData}
+                    labels={({ datum }) => `${datum.x}: ${datum.y}%`}
+                    padding={{
+                        bottom: 20,
+                        left: 20,
+                        right: 145,
+                        top: 20,
+                    }}
+                    title={/* istanbul ignore next */ `${Math.round(((primary?.value ?? 0) / total) * 100)}%`}
+                    subTitle={/* istanbul ignore next */ primary?.key}
+                    width={/* istanbul ignore next */ viewWidth < 376 ? viewWidth : 376}
+                    height={/* istanbul ignore next */ viewWidth < 376 ? 150 : 200}
+                />
+            </div>
         </Card>
     )
 }
