9f90caf2ec9639de9c5aef60b75fe06cd0487f03
Sho Weimer <sweimer@redhat.com>
2020-11-30 21:43:50 -0500

    Donut charts (#67)
    * save progress
    
    * donut chart tests
    
    * clean up
    
    * bug fixes

---
+++ src/AcmCharts/AcmDonutChart/AcmDonutChart.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { axe } from 'jest-axe'
+import { AcmDonutChart } from './AcmDonutChart'
+
+const complianceData = [
+    { key: 'Compliant', value: 1, isPrimary: true },
+    { key: 'Non-compliant', value: 1, isDanger: true },
+]
+const podData = [
+    { key: 'Running', value: 90, isPrimary: true },
+    { key: 'Pending', value: 8 },
+    { key: 'Failed', value: 2, isDanger: true },
+]
+
+describe('AcmDonutChart', () => {
+    test('renders', () => {
+        const { getByTestId } = render(
+            <AcmDonutChart
+                title="Cluster compliance"
+                description="Overview of policy compliance status"
+                data={complianceData}
+            />
+        )
+        expect(getByTestId('cluster-compliance-chart')).toBeInTheDocument()
+    })
+    test('has zero accessibility defects', async () => {
+        const { container } = render(
+            <AcmDonutChart title="Pods" description="Overview of pod count and status" data={podData} />
+        )
+        expect(await axe(container)).toHaveNoViolations()
+    })
+})
