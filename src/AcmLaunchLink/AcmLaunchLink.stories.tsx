9cb07bd2475b75b63ae2914c4b0e7e7ae6f523a2
Sho Weimer <sweimer@redhat.com>
2020-12-06 16:32:00 -0500

    dynamic launch link feature

---
+++ src/AcmLaunchLink/AcmLaunchLink.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { AcmLaunchLink, LaunchLink } from './AcmLaunchLink'
+
+export default {
+    title: 'LaunchLink',
+    component: AcmLaunchLink,
+    argtypes: {
+        count: {
+            control: {
+                type: 'range',
+                options: {
+                    min: 0,
+                    max: 3,
+                },
+            },
+        },
+    },
+}
+
+export const DynamicLaunchLink = (args) => {
+    const links: LaunchLink[] = [
+        { id: 'grafana', text: 'Grafana', href: '/grafana' },
+        { id: 'logs', text: 'Kibana', href: '/kibana' },
+        { id: 'cloud-pak', text: 'IBM CP4MCM', href: 'https://www.ibm.com' },
+    ]
+
+    const displayLinks = links.slice(0, args.count)
+
+    return <AcmLaunchLink links={displayLinks} />
+}
+DynamicLaunchLink.args = { count: 3 }
