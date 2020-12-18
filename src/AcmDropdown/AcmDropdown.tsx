839582737f24567d6161e45cd2d90d618004bb67
Randy Bruno Piverger <randybruno424@gmail.com>
2020-12-18 11:20:02 -0500

    Prop update (#117)
    * Prop update
    
    * prettier
    
    * testing update
    
    * update to weird style

--- src/AcmDropdown/AcmDropdown.tsx
+++ src/AcmDropdown/AcmDropdown.tsx
@@ -24,6 +24,8 @@ export type AcmDropdownProps = Props & {
     tooltip?: string | React.ReactNode
     isKebab?: boolean
     onHover?: () => void
+    isPlain?: boolean
+    isPrimary?: boolean
 }
 
 export type AcmDropdownItems = {
@@ -83,13 +85,18 @@ export function AcmDropdown(props: AcmDropdownProps) {
                     props.isKebab ? (
                         <KebabToggle id={props.id} isDisabled={props.isDisabled} onToggle={() => setOpen(!isOpen)} />
                     ) : (
-                        <DropdownToggle id={props.id} isDisabled={props.isDisabled} onToggle={() => setOpen(!isOpen)}>
+                        <DropdownToggle
+                            isPrimary={props.isPrimary}
+                            id={props.id}
+                            isDisabled={props.isDisabled}
+                            onToggle={() => setOpen(!isOpen)}
+                        >
                             {props.text}
                         </DropdownToggle>
                     )
                 }
                 isOpen={isOpen}
-                isPlain
+                isPlain={props.isPlain}
             />
         </TooltipWrapper>
     )
