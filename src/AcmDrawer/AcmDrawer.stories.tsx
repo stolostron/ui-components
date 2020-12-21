5c4b45a5e8308c53e4c911b5173188c92e3c88f3
Sho Weimer <sweimer@redhat.com>
2020-12-21 09:47:18 -0500

    Drawer component (#126)
    * prevent primary toggle from getting squished
    
    * drawer component

---
+++ src/AcmDrawer/AcmDrawer.stories.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import { ActionGroup, ButtonVariant } from '@patternfly/react-core'
+import { AcmButton } from '../AcmButton/AcmButton'
+import { AcmDrawer } from './AcmDrawer'
+import { AcmForm } from '../AcmForm/AcmForm'
+import { AcmLabelsInput } from '../AcmLabelsInput/AcmLabelsInput'
+
+export default {
+    title: 'Drawer',
+    component: AcmDrawer,
+}
+
+export const Drawer = () => {
+    const [isExpanded, setIsExpanded] = useState<boolean>(false)
+    const [labels, setLabels] = useState<Record<string, string>>({
+        cloud: 'Amazon',
+        clusterID: '1b1ghj3c-3a94-4fd3-awec-f3asdfsdff3',
+        name: 'local-cluster',
+        vendor: 'OpenShift',
+    })
+    return (
+        <>
+            <AcmDrawer
+                title="Edit Labels"
+                isExpanded={isExpanded}
+                onCloseClick={() => setIsExpanded(false)}
+                panelContent={
+                    <div>
+                        <p>
+                            Labels help you organize and select resources. Adding labels allows you to query for objects
+                            by using the labels. Selecting labels during policy and application creation allows you to
+                            distribute your resources to different clusters that share common labels.
+                        </p>
+                        <AcmForm style={{ marginTop: '24px' }}>
+                            <AcmLabelsInput
+                                label="local-cluster labels"
+                                id="labels-input"
+                                value={labels}
+                                onChange={(labels) => setLabels(labels)}
+                                buttonLabel="Add label"
+                            />
+                        </AcmForm>
+                        <ActionGroup style={{ marginTop: '24px' }}>
+                            <AcmButton onClick={() => setIsExpanded(!isExpanded)}>Save</AcmButton>
+                            <AcmButton onClick={() => setIsExpanded(!isExpanded)} variant={ButtonVariant.link}>
+                                Cancel
+                            </AcmButton>
+                        </ActionGroup>
+                    </div>
+                }
+            >
+                <div style={{ height: '100vh' }}>
+                    <AcmButton onClick={() => setIsExpanded(!isExpanded)}>Open drawer</AcmButton>
+                </div>
+            </AcmDrawer>
+        </>
+    )
+}
