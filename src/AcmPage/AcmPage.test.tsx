76c05a9823bd04870b3cf0ad63d108cdf5714f34
Sho Weimer <sweimer@redhat.com>
2021-01-11 13:13:47 -0500

    Optional tooltip prop for page titles (#148)
    * prevent primary toggle from getting squished
    
    * support page title tooltip

--- src/AcmPage/AcmPage.test.tsx
+++ src/AcmPage/AcmPage.test.tsx
@@ -24,6 +24,7 @@ describe('AcmPageHeader', () => {
                 <AcmPageHeader
                     breadcrumb={[{ text: 'text', to: 'to' }]}
                     title="title"
+                    titleTooltip="tooltip for title"
                     navigation="navigation"
                     controls="controls"
                     actions="actions"
