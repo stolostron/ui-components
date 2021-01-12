516120321a2117831159d51501161970ab170c54
James Talton <jtalton@redhat.com>
2021-01-12 11:33:19 -0500

    Alert Group: Clear by filter (#150)
    * Acm Alert Spacing
    
    * Support clearing alerts using a filter
    
    * Allow configurable alert margins

--- src/AcmAlert/AcmAlert.stories.tsx
+++ src/AcmAlert/AcmAlert.stories.tsx
@@ -86,11 +86,23 @@ export const AlertGroup = (args) => {
             </AcmButton>
         )
     }
+    const ClearInfoAlerts = () => {
+        const alertContext = useContext(AcmAlertContext)
+        return (
+            <AcmButton
+                onClick={() => {
+                    alertContext.clearAlerts((a) => a.type === 'info')
+                }}
+            >
+                Clear Info Alerts
+            </AcmButton>
+        )
+    }
     return (
         <AcmAlertProvider>
             <AcmPageCard>
-                <AcmAlertGroup isInline={args.isInline} canClose={args.canClose} />
-                <AddAlert /> <AddInfo /> <AddSuccess /> <AddWarning /> <AddError /> <ClearAlerts />
+                <AcmAlertGroup isInline={args.isInline} canClose={args.canClose} alertMargin="0 0 24px 0" />
+                <AddAlert /> <AddInfo /> <AddSuccess /> <AddWarning /> <AddError /> <ClearAlerts /> <ClearInfoAlerts />
             </AcmPageCard>
         </AcmAlertProvider>
     )
--- src/AcmAlert/AcmAlert.tsx
+++ src/AcmAlert/AcmAlert.tsx
@@ -1,6 +1,15 @@
 import Collapse from '@material-ui/core/Collapse'
 import { Alert, AlertActionCloseButton } from '@patternfly/react-core'
-import React, { createContext, HTMLAttributes, ReactNode, useCallback, useContext, useEffect, useState } from 'react'
+import React, {
+    createContext,
+    CSSProperties,
+    Fragment,
+    ReactNode,
+    useCallback,
+    useContext,
+    useEffect,
+    useState,
+} from 'react'
 
 export interface AcmAlertInfo {
     type?: 'success' | 'danger' | 'warning' | 'info' | 'default'
@@ -8,36 +17,49 @@ export interface AcmAlertInfo {
     message?: ReactNode
     actions?: ReactNode
     id?: string
+    group?: string
 }
 
 export interface IAlertContext {
+    readonly activeAlerts: AcmAlertInfo[]
     readonly alertInfos: AcmAlertInfo[]
     addAlert: (alertInfo: AcmAlertInfo) => void
     removeAlert: (alertInfo: AcmAlertInfo) => void
-    clearAlerts: () => void
-    readonly clear: number
+    removeVisibleAlert: (alertInfo: AcmAlertInfo) => void
+    clearAlerts: (matcher?: (alertInfo: AcmAlertInfo) => boolean) => void
 }
 
 /* istanbul ignore next */
 const noop = () => null
 
 export const AcmAlertContext = createContext<IAlertContext>({
+    activeAlerts: [],
     alertInfos: [],
     addAlert: noop,
     removeAlert: noop,
+    removeVisibleAlert: noop,
     clearAlerts: noop,
-    clear: 0,
 })
 
 export function AcmAlertProvider(props: { children: ReactNode }) {
-    const [alertInfos, setAlertInfos] = useState<AcmAlertInfo[]>([])
-    const [clear, setClear] = useState(0)
-    const addAlert = useCallback<(alertInfo: AcmAlertInfo) => void>((alertInfo: AcmAlertInfo) => {
-        alertInfo.id = Math.random().toString(36).substring(7)
-        setAlertInfos((alertInfos) => [...alertInfos, alertInfo])
+    const [activeAlerts, setActiveAlerts] = useState<AcmAlertInfo[]>([])
+    const [visibleAlerts, setVisibleAlerts] = useState<AcmAlertInfo[]>([])
+    const addAlert = useCallback<(alertInfo: AcmAlertInfo) => void>((alert: AcmAlertInfo) => {
+        alert.id = Math.random().toString(36).substring(7)
+        setActiveAlerts((alerts) => [...alerts, alert])
+        setVisibleAlerts((alerts) => [...alerts, alert])
     }, [])
     const removeAlert = useCallback<(alertInfo: AcmAlertInfo) => void>((alertInfo: AcmAlertInfo) => {
-        setAlertInfos((alertInfos) => {
+        setActiveAlerts((activeAlerts) => {
+            const index = activeAlerts.findIndex((ai) => ai.id === alertInfo.id)
+            const newAlertInfos = [...activeAlerts]
+            /* istanbul ignore else */
+            if (index !== -1) newAlertInfos.splice(index, 1)
+            return newAlertInfos
+        })
+    }, [])
+    const removeVisibleAlert = useCallback<(alertInfo: AcmAlertInfo) => void>((alertInfo: AcmAlertInfo) => {
+        setVisibleAlerts((alertInfos) => {
             const index = alertInfos.findIndex((ai) => ai.id === alertInfo.id)
             const newAlertInfos = [...alertInfos]
             /* istanbul ignore else */
@@ -45,9 +67,29 @@ export function AcmAlertProvider(props: { children: ReactNode }) {
             return newAlertInfos
         })
     }, [])
-    const clearAlerts = useCallback<() => void>(() => setClear((clear) => clear + 1), [])
+    const clearAlerts = (matcher?: (alertInfo: AcmAlertInfo) => boolean) => {
+        if (!matcher) {
+            for (const alertInfo of [...activeAlerts]) {
+                removeAlert(alertInfo)
+            }
+        } else {
+            const removeAlerts = activeAlerts.filter(matcher)
+            for (const alertInfo of removeAlerts) {
+                removeAlert(alertInfo)
+            }
+        }
+    }
     return (
-        <AcmAlertContext.Provider value={{ alertInfos, addAlert, removeAlert, clearAlerts, clear }}>
+        <AcmAlertContext.Provider
+            value={{
+                activeAlerts: activeAlerts,
+                alertInfos: visibleAlerts,
+                addAlert,
+                removeAlert,
+                removeVisibleAlert,
+                clearAlerts,
+            }}
+        >
             {props.children}
         </AcmAlertContext.Provider>
     )
@@ -61,55 +103,62 @@ export function AcmAlert(props: {
     message?: ReactNode
     noClose?: boolean
     variant?: 'success' | 'danger' | 'warning' | 'info' | 'default'
-    style?: HTMLAttributes<HTMLDivElement>
+    style?: CSSProperties
 }) {
     const alertContext = useContext(AcmAlertContext)
     const { alertInfo } = props
     const [open, setOpen] = useState(false)
-    const [clear] = useState(alertContext.clear)
     useEffect(() => setOpen(true), [])
     useEffect(() => {
-        if (clear !== alertContext.clear) setOpen(false)
+        if (alertInfo && !alertContext.activeAlerts.find((a) => a.id === alertInfo.id)) {
+            setOpen(false)
+        }
     }, [alertContext])
     return (
         <Collapse
-            style={props.style}
             in={open}
             onExit={() => {
                 /* istanbul ignore else */
                 if (alertInfo) {
-                    setTimeout(() => alertContext.removeAlert(alertInfo), 200)
+                    setTimeout(() => {
+                        alertContext.removeAlert(alertInfo)
+                        alertContext.removeVisibleAlert(alertInfo)
+                    }, 200)
                 }
             }}
             timeout={200}
         >
-            <div style={{ paddingBottom: '8px' }}>
-                <Alert
-                    isInline={props.isInline}
-                    title={alertInfo?.title || props.title}
-                    actionClose={!props.noClose && <AlertActionCloseButton onClose={() => setOpen(false)} />}
-                    variant={alertInfo?.type || props.variant}
-                >
-                    {alertInfo?.message || props.message || props.subtitle}
-                </Alert>
-            </div>
+            <Alert
+                isInline={props.isInline}
+                title={alertInfo?.title || props.title}
+                actionClose={!props.noClose && <AlertActionCloseButton onClose={() => setOpen(false)} />}
+                variant={alertInfo?.type || props.variant}
+                style={props.style}
+            >
+                {alertInfo?.message || props.message || props.subtitle}
+            </Alert>
         </Collapse>
     )
 }
 
-export function AcmAlertGroup(props: { isInline?: boolean; canClose?: boolean }) {
+export function AcmAlertGroup(props: { isInline?: boolean; canClose?: boolean; alertMargin?: string }) {
     const alertContext = useContext(AcmAlertContext)
     return alertContext.alertInfos.length > 0 ? (
-        <div style={{ paddingBottom: '16px' }}>
-            {alertContext.alertInfos.map((alertInfo) => (
-                <AcmAlert
-                    key={alertInfo.id}
-                    alertInfo={alertInfo}
-                    isInline={props.isInline}
-                    noClose={!props.canClose}
-                />
-            ))}
-        </div>
+        <Fragment>
+            {alertContext.alertInfos.map((alertInfo) => {
+                return (
+                    <AcmAlert
+                        key={alertInfo.id}
+                        alertInfo={alertInfo}
+                        isInline={props.isInline}
+                        noClose={!props.canClose}
+                        style={{
+                            margin: props.alertMargin,
+                        }}
+                    />
+                )
+            })}
+        </Fragment>
     ) : (
         <></>
     )
