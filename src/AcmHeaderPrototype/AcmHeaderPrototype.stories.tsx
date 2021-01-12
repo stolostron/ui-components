45c2f94df87f990cdba32ba8d4144cf4d7e1585e
Will Kutler <57921170+willkutler@users.noreply.github.com>
2021-01-12 15:03:46 -0500

    header ui component prototype (#152)
    * prototype header component
    
    * export new component
    
    * prototype test
    
    * click button
    
    * fix lint

---
+++ src/AcmHeaderPrototype/AcmHeaderPrototype.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { AcmHeaderPrototype } from './AcmHeaderPrototype'
+
+export default {
+    title: 'HeaderPrototype',
+    component: AcmHeaderPrototype,
+}
+
+export const ExpandableCard = () => {
+    return <AcmHeaderPrototype href="https://patternfly.org" target="_blank"></AcmHeaderPrototype>
+}
---
+++ src/AcmHeaderPrototype/AcmHeaderPrototype.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import { Page, PageHeader, PageHeaderTools, PageSidebar } from '@patternfly/react-core'
+
+export type AcmHeaderPrototypeProps = {
+    href: string
+    target: string
+}
+
+export function AcmHeaderPrototype(props: AcmHeaderPrototypeProps) {
+    const [isOpen, setOpen] = useState<boolean>(false)
+
+    const Header = (
+        <PageHeader
+            logo="Logo"
+            logoProps={props}
+            headerTools={<PageHeaderTools>header-tools</PageHeaderTools>}
+            showNavToggle
+            isNavOpen={isOpen}
+            onNavToggle={() => setOpen(!isOpen)}
+        />
+    )
+
+    const Sidebar = <PageSidebar nav="Navigation" isNavOpen={isOpen} />
+
+    return <Page header={Header} sidebar={Sidebar}></Page>
+}
