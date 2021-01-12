aa29d984e2ce95729a355b53bbbd336c0db83b39
Will <willkutler@gmail.com>
2021-01-12 11:09:40 -0700

    click button

--- src/AcmHeaderPrototype/AcmHeaderPrototype.test.tsx
+++ src/AcmHeaderPrototype/AcmHeaderPrototype.test.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import { axe } from 'jest-axe'
 import { AcmHeaderPrototype } from './AcmHeaderPrototype'
+import userEvent from '@testing-library/user-event'
 
 describe('AcmHeaderPrototype', () => {
     test('renders', async () => {
-        const { container } = render(
+        const { container, getByRole } = render(
             <AcmHeaderPrototype href="https://patternfly.org" target="_blank"></AcmHeaderPrototype>
         )
         expect(await axe(container)).toHaveNoViolations()
+        userEvent.click(getByRole('button'))
     })
 })
