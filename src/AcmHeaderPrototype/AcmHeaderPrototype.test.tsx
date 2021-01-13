a92b4fdd7a5a56606295459a0864a1126938f862
Will <willkutler@gmail.com>
2021-01-13 15:54:39 -0700

    remove unused vars

--- src/AcmHeaderPrototype/AcmHeaderPrototype.test.tsx
+++ src/AcmHeaderPrototype/AcmHeaderPrototype.test.tsx
@@ -3,11 +3,10 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import { axe } from 'jest-axe'
 import { AcmHeaderPrototype } from './AcmHeaderPrototype'
-import userEvent from '@testing-library/user-event'
 
 describe('AcmHeaderPrototype', () => {
     test('renders', async () => {
-        const { container, getByText } = render(
+        const { container } = render(
             <AcmHeaderPrototype href="https://patternfly.org" target="_blank">
                 <div>test</div>
             </AcmHeaderPrototype>
