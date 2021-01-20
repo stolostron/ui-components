f70eec7e7bf2da79ea8de1dc6a6a266d19a39d7d
James Talton <jtalton@redhat.com>
2021-01-20 13:24:39 -0500

    label collapse support

--- src/AcmLabels/AcmLabels.stories.tsx
+++ src/AcmLabels/AcmLabels.stories.tsx
@@ -15,7 +15,10 @@ export const Labels = () => (
                 abc: '123',
                 empty: '',
                 test: 'this is a test of a really long label. it is really long. abcdefghijklmnopqrstuvwxyz',
+                hidden: 'this is a hidden label',
+                hidden2: 'this is another hidden label',
             }}
+            collapse={['hidden', 'hidden2']}
         />
     </AcmPageCard>
 )
--- src/AcmLabels/AcmLabels.test.tsx
+++ src/AcmLabels/AcmLabels.test.tsx
@@ -6,12 +6,19 @@ import { AcmLabels } from './AcmLabels'
 describe('AcmLabels', () => {
     test('renders empty with undefined', () => {
         const { container } = render(<AcmLabels labels={undefined} />)
-        expect(container).toMatchInlineSnapshot('<div />')
+        expect(container).toMatchInlineSnapshot(`
+            <div>
+              <div
+                style="display: flex; flex-wrap: wrap;"
+              />
+            </div>
+        `)
     })
     test('renders with string array', () => {
-        const { getByText } = render(<AcmLabels labels={['foo=bar', 'cluster=management']} />)
+        const { getByText } = render(<AcmLabels labels={['foo=bar', 'cluster=management', 'test=']} />)
         expect(getByText('foo=bar')).toBeInTheDocument()
         expect(getByText('cluster=management')).toBeInstanceOf(HTMLSpanElement)
+        expect(getByText('test')).toBeInstanceOf(HTMLSpanElement)
     })
     test('renders with object', () => {
         const { getByText } = render(<AcmLabels labels={{ foo: 'bar', cluster: 'management', empty: '' }} />)
@@ -26,5 +33,15 @@ describe('AcmLabels', () => {
     test('has zero accessibility defects', async () => {
         const { container } = render(<AcmLabels labels={['foo=bar', 'cluster=management']} />)
         expect(await axe(container)).toHaveNoViolations()
+    })
+    test('renders with collapsed labels', () => {
+        const { getByText } = render(
+            <AcmLabels labels={{ foo: 'bar', cluster: 'management', empty: '' }} collapse={['cluster', 'empty']} />
+        )
+        expect(getByText('foo=bar')).toBeInTheDocument()
+        expect(getByText('+2')).toBeInTheDocument()
+        getByText('+2').click()
+        expect(getByText('cluster=management')).toBeInstanceOf(HTMLSpanElement)
+        expect(getByText('empty')).toBeInTheDocument()
     })
 })
