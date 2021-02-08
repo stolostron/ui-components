30cec6d81584dc736e190b19e4cd536f7dab985f
James Talton <jtalton@redhat.com>
2021-02-08 13:16:50 -0500

    AcmButton LinkProps support

--- src/AcmButton/AcmButton.tsx
+++ src/AcmButton/AcmButton.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Button, ButtonProps } from '@patternfly/react-core'
 import { TooltipWrapper } from '../utils'
+import { LinkProps } from 'react-router-dom'
 
-export function AcmButton(props: ButtonProps & { tooltip?: string | React.ReactNode }) {
+export function AcmButton(props: ButtonProps & { tooltip?: string | React.ReactNode } & LinkProps) {
     const { isDisabled, tooltip, children, ...otherProps } = props
     return (
         <TooltipWrapper showTooltip={isDisabled && !!tooltip} tooltip={tooltip}>
