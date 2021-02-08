a2ad7dc83083d5ef2ad7b9e42cadb52b0250a1fc
James Talton <jtalton@redhat.com>
2021-02-08 14:53:16 -0500

    fix test

--- src/AcmButton/AcmButton.tsx
+++ src/AcmButton/AcmButton.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
 import { Button, ButtonProps } from '@patternfly/react-core'
+import { Location, LocationDescriptor, LocationState } from 'history'
+import React from 'react'
 import { TooltipWrapper } from '../utils'
-import { LinkProps } from 'react-router-dom'
 
-export function AcmButton(props: ButtonProps & { tooltip?: string | React.ReactNode } & Partial<LinkProps>) {
+export function AcmButton(
+    props: ButtonProps & {
+        tooltip?: string | React.ReactNode
+        to?:
+            | LocationDescriptor<LocationState>
+            | ((location: Location<LocationState>) => LocationDescriptor<LocationState>)
+    }
+) {
     const { isDisabled, tooltip, children, ...otherProps } = props
     return (
         <TooltipWrapper showTooltip={isDisabled && !!tooltip} tooltip={tooltip}>
