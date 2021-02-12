11d2d5abafda9b1a3ebf5be3e021af368e86f393
Will <willkutler@gmail.com>
2021-02-12 11:07:18 -0700

    remove unnecessary header and open console in same tab

--- src/AcmHeaderPrototype/AcmHeaderPrototype.tsx
+++ src/AcmHeaderPrototype/AcmHeaderPrototype.tsx
@@ -83,23 +83,6 @@ function AboutModalVersion() {
     return <span className="version-details__no">{version === 'undefined' ? <Spinner size="md" /> : version}</span>
 }
 
-//helper function to get auth token from cookie
-function getAuthToken(): string {
-    if (!document.cookie) {
-        return ''
-    }
-
-    const xsrfCookies = document.cookie
-        .split(';')
-        .map((c) => c.trim())
-        .filter((c) => c.startsWith('acm-access-token-cookie='))
-
-    if (xsrfCookies.length === 0) {
-        return ''
-    }
-    return decodeURIComponent(xsrfCookies[0].split('=')[1])
-}
-
 function NavExpandableList() {
     const [activeGroup, setActiveGroup] = useState<string>('grp-1')
     const [activeItem, setActiveItem] = useState<string>('grp-1_itm-1')
@@ -115,19 +98,9 @@ function NavExpandableList() {
     }
 
     function launchToOCP(searchParam: string) {
-        api<{ data: { consoleURL: string } }>(
-            '/multicloud/api/v1/namespaces/openshift-config-managed/configmaps/console-public',
-            {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Accept: 'application/json',
-                    Authorization: `Bearer ${getAuthToken()}`,
-                },
-            }
-        )
+        api<{ data: { consoleURL: string } }>('/multicloud/api/v1/namespaces/openshift-config-managed/configmaps/console-public')
             .then(({ data }) => {
-                window.open(`${data.consoleURL}${searchParam}`, '_blank')
+                window.open(`${data.consoleURL}${searchParam}`, '_self')
             })
             .catch((error) => {
                 // eslint-disable-next-line no-console
@@ -161,6 +134,7 @@ function NavExpandableList() {
             <NavList>
                 <NavItem
                     preventDefault
+                    to="#mixed-1"
                     groupId="home"
                     itemId="home_welcome"
                     isActive={activeItem === 'home_welcome'}
@@ -171,6 +145,7 @@ function NavExpandableList() {
                 <NavExpandable title="Observe Environments" groupId="observe" isActive={activeGroup === 'observe'}>
                     <NavItem
                         preventDefault
+                        to="#mixed-2"
                         groupId="observe"
                         itemId="observe_overview"
                         isActive={activeItem === 'observe_overview'}
@@ -182,6 +157,7 @@ function NavExpandableList() {
                 <NavExpandable title="Automate Infrastructure" groupId="automate" isActive={activeGroup === 'automate'}>
                     <NavItem
                         preventDefault
+                        to="#mixed-3"
                         groupId="automate"
                         itemId="automate_clusters"
                         isActive={activeItem === 'automate_clusters'}
@@ -191,6 +167,7 @@ function NavExpandableList() {
                     </NavItem>
                     <NavItem
                         preventDefault
+                        to="#mixed-4"
                         groupId="automate"
                         itemId="automate_baremetal"
                         isActive={activeItem === 'automate_baremetal'}
@@ -201,6 +178,7 @@ function NavExpandableList() {
                 </NavExpandable>
                 <NavItem
                     preventDefault
+                    to="#mixed-5"
                     groupId="manage"
                     itemId="manage_applications"
                     isActive={activeItem === 'manage_applications'}
@@ -210,6 +188,7 @@ function NavExpandableList() {
                 </NavItem>
                 <NavItem
                     preventDefault
+                    to="#mixed-6"
                     groupId="grc"
                     itemId="grc_govern_risk"
                     isActive={activeItem === 'grc_govern_risk'}
