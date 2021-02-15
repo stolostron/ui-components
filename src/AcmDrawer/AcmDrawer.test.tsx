955f892f81daac0f393011feeb0353c07f3083b6
Sho Weimer <sweimer@redhat.com>
2021-02-15 14:30:52 -0500

    AcmDrawer - build context features and plug them into AcmPage component (#209)
    * update acm labels
    
    * build drawer context provider, and build it into the AcmPage

--- src/AcmDrawer/AcmDrawer.test.tsx
+++ src/AcmDrawer/AcmDrawer.test.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { axe } from 'jest-axe'
-import { AcmDrawer } from './AcmDrawer'
+import { AcmDrawer, AcmDrawerContext, AcmDrawerProvider } from './AcmDrawer'
+import { AcmButton } from '../AcmButton/AcmButton'
 
 describe('AcmDrawer', () => {
     const onCloseClick = jest.fn()
@@ -17,5 +19,43 @@ describe('AcmDrawer', () => {
         const { container } = render(<AcmDrawer title="Drawer" onCloseClick={onCloseClick} isExpanded={true} />)
         expect(screen.getByText('Drawer')).toBeInTheDocument()
         expect(await axe(container)).toHaveNoViolations()
+    })
+
+    describe('using context', () => {
+        const Component = () => (
+            <AcmDrawerProvider>
+                <AcmDrawer>
+                    <AcmDrawerContext.Consumer>
+                        {({ setDrawerContext }) => (
+                            <div style={{ height: '100vh' }}>
+                                <AcmButton
+                                    onClick={() =>
+                                        setDrawerContext({
+                                            isExpanded: true,
+                                            title: 'Drawer title',
+                                            onCloseClick: () => setDrawerContext(undefined),
+                                            panelContent: <div id="test-content" />,
+                                        })
+                                    }
+                                >
+                                    Open
+                                </AcmButton>
+                            </div>
+                        )}
+                    </AcmDrawerContext.Consumer>
+                </AcmDrawer>
+            </AcmDrawerProvider>
+        )
+        test('', async () => {
+            render(<Component />)
+            expect(screen.queryByText('Drawer title')).toBeNull()
+            expect(screen.getByText('Open')).toBeInTheDocument()
+            userEvent.click(screen.getByText('Open'))
+            await waitFor(() => expect(screen.getByText('Drawer title')).toBeInTheDocument())
+            expect(screen.getByTestId('test-content')).toBeInTheDocument()
+            expect(screen.getByLabelText('Close drawer panel')).toBeInTheDocument()
+            userEvent.click(screen.getByLabelText('Close drawer panel'))
+            await waitFor(() => expect(screen.queryByText('Drawer title')).toBeNull())
+        })
     })
 })
