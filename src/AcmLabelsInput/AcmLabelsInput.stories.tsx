2a1e1ae63e51f71734cf25181577c333e5c17a47
Sho Weimer <sweimer@redhat.com>
2021-02-16 16:06:44 -0500

    Allow disable feature on AcmLabelsInput (#213)
    * Allow disable feature on AcmLabelsInput
    
    * ignore

--- src/AcmLabelsInput/AcmLabelsInput.stories.tsx
+++ src/AcmLabelsInput/AcmLabelsInput.stories.tsx
@@ -2,24 +2,32 @@ import '@patternfly/react-core/dist/styles/base.css'
 import React, { useState } from 'react'
 import { AcmForm } from '../AcmForm/AcmForm'
 import { AcmLabelsInput } from './AcmLabelsInput'
+import { AcmPageCard } from '../AcmPage/AcmPage'
 
 export default {
     title: 'LabelsInput',
     component: AcmLabelsInput,
+    argTypes: {
+        isDisabled: { control: 'boolean' },
+    },
 }
 
-export const LabelsInput = () => {
+export const LabelsInput = (args) => {
     const [value, setValue] = useState<string[]>([])
     return (
-        <AcmForm>
-            <AcmLabelsInput
-                label="Labels Input"
-                id="labels-input"
-                value={value}
-                onChange={setValue}
-                buttonLabel="Add label"
-                placeholder="Enter key=value, then press enter, space, or comma"
-            />
-        </AcmForm>
+        <AcmPageCard>
+            <AcmForm>
+                <AcmLabelsInput
+                    label="Labels Input"
+                    id="labels-input"
+                    isDisabled={args.isDisabled}
+                    value={value}
+                    onChange={setValue}
+                    buttonLabel="Add label"
+                    placeholder="Enter key=value, then press enter, space, or comma"
+                />
+            </AcmForm>
+        </AcmPageCard>
     )
 }
+LabelsInput.args = { isDisabled: false }
