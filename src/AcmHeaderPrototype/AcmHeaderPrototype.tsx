137b39aa253d235fa775974007b940b8afb53711
Will <willkutler@gmail.com>
2021-03-11 11:30:48 -0700

    lint

--- src/AcmHeaderPrototype/AcmHeaderPrototype.tsx
+++ src/AcmHeaderPrototype/AcmHeaderPrototype.tsx
@@ -28,12 +28,9 @@ import {
     TextList,
     Title,
 } from '@patternfly/react-core'
-import { CogsIcon, CodeIcon } from '@patternfly/react-icons'
+import { CogsIcon, CodeIcon, CaretDownIcon } from '@patternfly/react-icons'
 import { makeStyles } from '@material-ui/styles'
 import logo from '../assets/RHACM-Logo.svg'
-import {
-    CaretDownIcon,
-  } from '@patternfly/react-icons'
 
 export type AcmHeaderPrototypeProps = {
     href: string
@@ -100,7 +97,7 @@ function UserDropdownToggle() {
 }
 
 type AboutDropdownProps = {
-    aboutClick: Function
+    aboutClick: () => void
 }
 function AboutDropdown(props: AboutDropdownProps) {
     const [aboutDDIsOpen, aboutDDSetOpen] = useState<boolean>(false)
@@ -110,14 +107,14 @@ function AboutDropdown(props: AboutDropdownProps) {
             <ApplicationLauncherItem href="https://access.redhat.com/documentation/en-us/red_hat_advanced_cluster_management_for_kubernetes/2.2/">
                 Documentation
             </ApplicationLauncherItem>
-          )
+        )
     }
     function AboutButton() {
         return (
             <ApplicationLauncherItem component="button" onClick={() => props.aboutClick()}>
                 About
             </ApplicationLauncherItem>
-          )
+        )
     }
 
     return (
@@ -128,25 +125,31 @@ function AboutDropdown(props: AboutDropdownProps) {
             onSelect={() => aboutDDSetOpen(false)}
             onToggle={() => aboutDDSetOpen(!aboutDDIsOpen)}
             isOpen={aboutDDIsOpen}
-            items={[
-                <DocsButton key="docs" />,
-                <AboutButton key="about_modal_button"/>
-            ]}
+            items={[<DocsButton key="docs" />, <AboutButton key="about_modal_button" />]}
             data-quickstart-id="qs-masthead-helpmenu"
             position="right"
             toggleIcon={
                 <svg width="18px" height="20px" viewBox="0 0 18 20" version="1.1">
                     <title>help-icon</title>
                     <g id="Help" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-                        <g id="01.00-Help" transform="translate(-1277.000000, -29.000000)" fill="#EDEDED" fillRule="nonzero">
+                        <g
+                            id="01.00-Help"
+                            transform="translate(-1277.000000, -29.000000)"
+                            fill="#EDEDED"
+                            fillRule="nonzero"
+                        >
                             <g id="help-icon" transform="translate(1277.000000, 29.381579)">
-                                <path d="M9.00103711,0.0025467465 C4.03806445,0.0025467465 0,4.09288877 0,9.12141303 C0,14.1478536 4.03806445,18.2393889 9.00103711,18.2393889 C13.9630078,18.2393889 18,14.1479426 18,9.12141303 C18,4.09288877 13.9630254,0.0025467465 9.00103711,0.0025467465 Z M9.00103711,16.0250729 C5.24237695,16.0250729 2.18550586,12.9287991 2.18550586,9.12137742 C2.18550586,5.3121214 5.24241211,2.21677364 9.00103711,2.21677364 C12.7577812,2.21677364 15.8155664,5.31210359 15.8155664,9.12137742 C15.8155664,12.9287634 12.7577285,16.0250729 9.00103711,16.0250729 Z M10.2857168,4.23609429 L10.2857168,6.19003657 C10.2857168,6.27821099 10.2539355,6.35454215 10.1902852,6.41901223 C10.1266348,6.48348232 10.0513125,6.51569955 9.9642832,6.51569955 L8.0357168,6.51569955 C7.94865234,6.51569955 7.8733125,6.48350013 7.80971484,6.41901223 C7.74611719,6.35452434 7.7142832,6.27821099 7.7142832,6.19003657 L7.7142832,4.23609429 C7.7142832,4.14791987 7.74609961,4.07158871 7.80971484,4.00711863 C7.87333008,3.94264854 7.94865234,3.91043131 8.0357168,3.91043131 L9.9642832,3.91043131 C10.0513125,3.91043131 10.1266523,3.94263073 10.1902852,4.00711863 C10.253918,4.07160652 10.2857168,4.14791987 10.2857168,4.23609429 L10.2857168,4.23609429 Z M9.16903125,7.81833368 C11.2492793,7.81833368 12.9357773,9.40274838 12.9357773,11.1563347 C12.9357773,12.9099211 11.2492793,14.3315043 9.16903125,14.3315043 C6.12722461,14.3315043 5.51118164,12.6625127 5.40796289,11.2840817 C5.40796289,11.1427461 5.52624609,11.1088726 5.68696289,11.1088726 C5.84767969,11.1088726 7.58668359,11.1088726 7.67580469,11.1088726 C7.78296094,11.1088726 7.93024805,11.120146 7.98437109,11.297136 C7.98437109,12.1672506 10.3744336,12.2758168 10.3744336,11.1563703 C10.3744336,10.5952669 9.85206445,10.0198268 9.16908398,9.97749386 C8.48610352,9.93516088 7.72265039,9.84201763 7.72265039,9.01224131 C7.72265039,8.7803271 7.72265039,8.55502017 7.72265039,8.21628508 C7.72265039,7.87763903 7.89345703,7.81833368 8.20483594,7.81833368 C8.51621484,7.81833368 9.16904883,7.81833368 9.16904883,7.81833368 L9.16903125,7.81833368 Z" id="Shape" transform="translate(9.000000, 9.120968) scale(-1, 1) rotate(-180.000000) translate(-9.000000, -9.120968) "></path>
+                                <path
+                                    d="M9.00103711,0.0025467465 C4.03806445,0.0025467465 0,4.09288877 0,9.12141303 C0,14.1478536 4.03806445,18.2393889 9.00103711,18.2393889 C13.9630078,18.2393889 18,14.1479426 18,9.12141303 C18,4.09288877 13.9630254,0.0025467465 9.00103711,0.0025467465 Z M9.00103711,16.0250729 C5.24237695,16.0250729 2.18550586,12.9287991 2.18550586,9.12137742 C2.18550586,5.3121214 5.24241211,2.21677364 9.00103711,2.21677364 C12.7577812,2.21677364 15.8155664,5.31210359 15.8155664,9.12137742 C15.8155664,12.9287634 12.7577285,16.0250729 9.00103711,16.0250729 Z M10.2857168,4.23609429 L10.2857168,6.19003657 C10.2857168,6.27821099 10.2539355,6.35454215 10.1902852,6.41901223 C10.1266348,6.48348232 10.0513125,6.51569955 9.9642832,6.51569955 L8.0357168,6.51569955 C7.94865234,6.51569955 7.8733125,6.48350013 7.80971484,6.41901223 C7.74611719,6.35452434 7.7142832,6.27821099 7.7142832,6.19003657 L7.7142832,4.23609429 C7.7142832,4.14791987 7.74609961,4.07158871 7.80971484,4.00711863 C7.87333008,3.94264854 7.94865234,3.91043131 8.0357168,3.91043131 L9.9642832,3.91043131 C10.0513125,3.91043131 10.1266523,3.94263073 10.1902852,4.00711863 C10.253918,4.07160652 10.2857168,4.14791987 10.2857168,4.23609429 L10.2857168,4.23609429 Z M9.16903125,7.81833368 C11.2492793,7.81833368 12.9357773,9.40274838 12.9357773,11.1563347 C12.9357773,12.9099211 11.2492793,14.3315043 9.16903125,14.3315043 C6.12722461,14.3315043 5.51118164,12.6625127 5.40796289,11.2840817 C5.40796289,11.1427461 5.52624609,11.1088726 5.68696289,11.1088726 C5.84767969,11.1088726 7.58668359,11.1088726 7.67580469,11.1088726 C7.78296094,11.1088726 7.93024805,11.120146 7.98437109,11.297136 C7.98437109,12.1672506 10.3744336,12.2758168 10.3744336,11.1563703 C10.3744336,10.5952669 9.85206445,10.0198268 9.16908398,9.97749386 C8.48610352,9.93516088 7.72265039,9.84201763 7.72265039,9.01224131 C7.72265039,8.7803271 7.72265039,8.55502017 7.72265039,8.21628508 C7.72265039,7.87763903 7.89345703,7.81833368 8.20483594,7.81833368 C8.51621484,7.81833368 9.16904883,7.81833368 9.16904883,7.81833368 L9.16903125,7.81833368 Z"
+                                    id="Shape-help"
+                                    transform="translate(9.000000, 9.120968) scale(-1, 1) rotate(-180.000000) translate(-9.000000, -9.120968) "
+                                ></path>
                             </g>
                         </g>
                     </g>
                 </svg>
             }
-      />
+        />
     )
 }
 
@@ -198,14 +201,14 @@ function UserDropdown() {
             <ApplicationLauncherItem component="button" onClick={() => logout()}>
                 Logout
             </ApplicationLauncherItem>
-          )
+        )
     }
     function ConfigureButton() {
         return (
             <ApplicationLauncherItem component="button" onClick={() => configureClient()}>
                 Configure client
             </ApplicationLauncherItem>
-          )
+        )
     }
 
     return (
@@ -216,14 +219,11 @@ function UserDropdown() {
             onSelect={() => userSetOpen(false)}
             onToggle={() => userSetOpen(!userIsOpen)}
             isOpen={userIsOpen}
-            items={[
-                <LogoutButton key="user_logout" />,
-                <ConfigureButton key="user_configure"/>
-            ]}
+            items={[<LogoutButton key="user_logout" />, <ConfigureButton key="user_configure" />]}
             data-quickstart-id="qs-masthead-usermenu"
             position="right"
             toggleIcon={<UserDropdownToggle />}
-      />
+        />
     )
 }
 
@@ -257,7 +257,7 @@ function AboutContent() {
                 </TextListItem>
             </TextList>
         </TextContent>
-      )
+    )
 }
 
 const useStyles = makeStyles({
@@ -274,24 +274,24 @@ const useStyles = makeStyles({
         },
     },
     about: {
-        height: 'min-content'
+        height: 'min-content',
     },
     perspective: {
         'font-size': '$co-side-nav-font-size',
         'justify-content': 'space-between',
         width: '100%',
-    
+
         '& .pf-c-dropdown__toggle-icon': {
             color: 'var(--pf-global--Color--light-100)',
             'font-size': '$co-side-nav-section-font-size',
         },
-        
+
         '& .pf-c-dropdown__menu-item': {
             'padding-left': '7px',
             '& h2': {
                 'font-size': '12px',
                 'padding-left': '7px',
-            }
+            },
         },
 
         '& .pf-c-title': {
@@ -313,11 +313,11 @@ const useStyles = makeStyles({
             width: '1em',
             fill: 'currentColor',
         },
-    
+
         '&::before': {
             border: 'none',
-        }
-    }
+        },
+    },
 })
 
 type NavExpandableProps = {
@@ -426,7 +426,7 @@ function NavExpandableList(props: NavExpandableProps) {
         paddingRight: '7px',
     }
     const ddStyles: CSSProperties = {
-        padding: 'var(--pf-global--spacer--sm) var(--pf-global--spacer--sm)'
+        padding: 'var(--pf-global--spacer--sm) var(--pf-global--spacer--sm)',
     }
 
     return (
@@ -434,7 +434,7 @@ function NavExpandableList(props: NavExpandableProps) {
             <div className="oc-nav-header" style={ddStyles}>
                 <Dropdown
                     toggle={
-                        <DropdownToggle 
+                        <DropdownToggle
                             id="toggle-perspective"
                             onToggle={() => switcherSetOpen(!switcherIsOpen)}
                             toggleIndicator={CaretDownIcon}
@@ -443,7 +443,13 @@ function NavExpandableList(props: NavExpandableProps) {
                         >
                             <Title headingLevel="h2" size="md">
                                 <span className="oc-nav-header__icon">
-                                    <svg viewBox="0 0 14 13.97"><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path d="M12.63,6A1.5,1.5,0,1,0,11,4.51l-1.54.91a2.94,2.94,0,0,0-1.85-1L7.35,2.66a1.52,1.52,0,0,0,.49-.72,1.5,1.5,0,0,0-1-1.87A1.49,1.49,0,0,0,5,1.06a1.51,1.51,0,0,0,.88,1.83L6.12,4.6A2.9,2.9,0,0,0,4.5,6.29L2.88,6.07a1.52,1.52,0,0,0-.55-.68,1.51,1.51,0,0,0-2.08.43A1.49,1.49,0,0,0,2.67,7.56l1.68.23A3,3,0,0,0,5.41,9.6L4.8,11a1.5,1.5,0,1,0,1.14,2.63,1.49,1.49,0,0,0,.24-2l.61-1.39a3.44,3.44,0,0,0,.45,0,2.92,2.92,0,0,0,1.6-.48L10.21,11a1.45,1.45,0,0,0,.09.87,1.5,1.5,0,1,0,.91-2L9.85,8.66a3,3,0,0,0,.33-1.34,3.1,3.1,0,0,0,0-.54l1.64-1A1.47,1.47,0,0,0,12.63,6ZM5.48,7.32A1.77,1.77,0,1,1,7.24,9.08,1.76,1.76,0,0,1,5.48,7.32Z"/></g></g></svg>
+                                    <svg viewBox="0 0 14 13.97">
+                                        <g id="Layer_2" data-name="Layer 2">
+                                            <g id="Layer_1-2" data-name="Layer 1">
+                                                <path d="M12.63,6A1.5,1.5,0,1,0,11,4.51l-1.54.91a2.94,2.94,0,0,0-1.85-1L7.35,2.66a1.52,1.52,0,0,0,.49-.72,1.5,1.5,0,0,0-1-1.87A1.49,1.49,0,0,0,5,1.06a1.51,1.51,0,0,0,.88,1.83L6.12,4.6A2.9,2.9,0,0,0,4.5,6.29L2.88,6.07a1.52,1.52,0,0,0-.55-.68,1.51,1.51,0,0,0-2.08.43A1.49,1.49,0,0,0,2.67,7.56l1.68.23A3,3,0,0,0,5.41,9.6L4.8,11a1.5,1.5,0,1,0,1.14,2.63,1.49,1.49,0,0,0,.24-2l.61-1.39a3.44,3.44,0,0,0,.45,0,2.92,2.92,0,0,0,1.6-.48L10.21,11a1.45,1.45,0,0,0,.09.87,1.5,1.5,0,1,0,.91-2L9.85,8.66a3,3,0,0,0,.33-1.34,3.1,3.1,0,0,0,0-.54l1.64-1A1.47,1.47,0,0,0,12.63,6ZM5.48,7.32A1.77,1.77,0,1,1,7.24,9.08,1.76,1.76,0,0,1,5.48,7.32Z" />
+                                            </g>
+                                        </g>
+                                    </svg>
                                 </span>
                                 Advanced Cluster Management
                             </Title>
@@ -452,13 +458,17 @@ function NavExpandableList(props: NavExpandableProps) {
                     dropdownItems={[
                         <DropdownItem onClick={() => launchToOCP('?perspective=admin')} key={'administrator'}>
                             <Title headingLevel="h2" size="md">
-                                <span className="oc-nav-header__icon" style={iconStyles}><CogsIcon></CogsIcon></span>
+                                <span className="oc-nav-header__icon" style={iconStyles}>
+                                    <CogsIcon></CogsIcon>
+                                </span>
                                 Administrator
                             </Title>
                         </DropdownItem>,
                         <DropdownItem onClick={() => launchToOCP('?perspective=dev')} key={'devbutton'}>
                             <Title headingLevel="h2" size="md">
-                                <span className="oc-nav-header__icon" style={iconStyles}><CodeIcon></CodeIcon></span>
+                                <span className="oc-nav-header__icon" style={iconStyles}>
+                                    <CodeIcon></CodeIcon>
+                                </span>
                                 Developer
                             </Title>
                         </DropdownItem>,
@@ -545,7 +555,7 @@ export function AcmHeaderPrototype(props: AcmHeaderPrototypeProps) {
                                         <g id="add-resource-icon" transform="translate(1177.000000, 29.381579)">
                                             <path
                                                 d="M9.00103711,0 C4.03806445,0 0,4.09034203 0,9.11886629 C0,14.1453603 4.03806445,18.2368421 9.00103711,18.2368421 C13.963043,18.2368421 18,14.1453959 18,9.11886629 C18,4.09034203 13.9630254,0 9.00103711,0 Z M9.00103711,16.0225262 C5.24237695,16.0225262 2.18550586,12.9262523 2.18550586,9.11883067 C2.18550586,5.30961027 5.24241211,2.21428032 9.00103711,2.21428032 C12.7577812,2.21428032 15.8155664,5.30961027 15.8155664,9.11883067 C15.8155664,12.9262167 12.7577285,16.0225262 9.00103711,16.0225262 Z M12.7666934,10.3280366 C12.706418,10.3899599 12.6295312,10.4210552 12.535752,10.4210552 L10.2856641,10.4210552 L10.2856641,12.7007851 C10.2856641,12.7957627 10.2550957,12.8737145 10.1939414,12.9347652 C10.1327871,12.9958159 10.0544414,13.0263234 9.95925586,13.0263234 L8.04072656,13.0263234 C7.94550586,13.0263234 7.86735352,12.9957981 7.80604102,12.9347652 C7.74485156,12.8736967 7.71426562,12.7957093 7.71426562,12.7007317 L7.71426562,10.4210374 L5.46414258,10.4210017 C5.37039844,10.4210017 5.29345898,10.3900668 5.23320117,10.3280722 C5.17297852,10.2661489 5.14283203,10.1867723 5.14283203,10.0902987 L5.14283203,8.1465256 C5.14283203,8.05005199 5.17296094,7.97099599 5.23320117,7.90875208 C5.29347656,7.8467575 5.37045117,7.81576912 5.46419531,7.81576912 L7.7142832,7.81580474 L7.7142832,5.53603919 C7.7142832,5.44102596 7.74493945,5.36316319 7.80612891,5.30213032 C7.86724805,5.24106183 7.9454707,5.21050087 8.04069141,5.21050087 L9.9592207,5.21050087 C10.0544414,5.21050087 10.1325059,5.24107964 10.1939062,5.30213032 C10.2550254,5.36319881 10.2857168,5.44109719 10.2857168,5.53611043 L10.2857168,7.81575131 L12.5358398,7.81575131 C12.6296191,7.81575131 12.7064707,7.84677531 12.7667109,7.90880551 C12.8269863,7.97072885 12.8571504,8.04998075 12.8571504,8.14645436 L12.8571504,10.0902275 C12.8571504,10.1867011 12.8269688,10.2658817 12.7667109,10.328001 L12.7666934,10.3280366 Z"
-                                                id="Shape"
+                                                id="Shape-addres"
                                                 transform="translate(9.000000, 9.118421) scale(-1, 1) rotate(-180.000000) translate(-9.000000, -9.118421) "
                                             ></path>
                                         </g>
@@ -558,7 +568,9 @@ export function AcmHeaderPrototype(props: AcmHeaderPrototypeProps) {
                     <AboutModal
                         isOpen={aboutModalOpen}
                         onClose={() => setAboutModalOpen(!aboutModalOpen)}
-                        trademark={"Copyright © 2020 IBM Corporation. All rights reserved. \nCopyright © 2020 Red Hat, Inc. All rights reserved."}
+                        trademark={
+                            'Copyright © 2020 IBM Corporation. All rights reserved. \nCopyright © 2020 Red Hat, Inc. All rights reserved.'
+                        }
                         brandImageSrc={logo}
                         brandImageAlt="ACM logo"
                         className={classes.about}
