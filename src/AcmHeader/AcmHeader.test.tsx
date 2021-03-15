33bd5d71681252415202bab473156c5ebd434687
James Talton <jtalton@redhat.com>
2021-03-15 17:30:14 -0400

    AcmHeader support react dom router navigation for console (#242)

--- src/AcmHeader/AcmHeader.test.tsx
+++ src/AcmHeader/AcmHeader.test.tsx
@@ -1,23 +1,27 @@
 /* Copyright Contributors to the Open Cluster Management project */
 
 /* istanbul ignore file */
-import React from 'react'
 import { render } from '@testing-library/react'
-import { axe } from 'jest-axe'
-import { AcmHeader } from './AcmHeader'
 import 'isomorphic-fetch'
+import { axe } from 'jest-axe'
 import nock from 'nock'
+import React from 'react'
+import { MemoryRouter } from 'react-router'
+import { AcmHeader } from './AcmHeader'
 
 describe('AcmHeader', () => {
     beforeAll(() => {
         const APIServer = nock('https://localhost:3000')
         APIServer.persist().get('/multicloud/common/username').reply(200, { username: 'kubeadmin' })
     })
+
     test('renders', async () => {
         const { container } = render(
-            <AcmHeader urlpath="/multicloud/policies/all" href="https://patternfly.org" target="_blank">
-                <div>test</div>
-            </AcmHeader>
+            <MemoryRouter>
+                <AcmHeader>
+                    <div>test</div>
+                </AcmHeader>
+            </MemoryRouter>
         )
         expect(await axe(container)).toHaveNoViolations()
     })
