8b801a735949337ba3c0f897d4552b7f439efdc4
Will <willkutler@gmail.com>
2021-03-18 11:44:28 -0600

    fix lint

--- src/AcmHeader/AcmHeader.tsx
+++ src/AcmHeader/AcmHeader.tsx
@@ -65,9 +65,7 @@ function launchToOCP(urlSuffix: string) {
 
 function checkOCPVersion(switcherExists: (arg0: boolean) => void) {
     if (process.env.NODE_ENV === 'test') return
-    api<{ data: { gitVersion: string } }>(
-        '/multicloud/version/'
-    )
+    api<{ data: { gitVersion: string } }>('/multicloud/version/')
         .then(({ data }) => {
             if (parseFloat(data.gitVersion.substr(1, 4)) >= 1.2) {
                 switcherExists(true)
