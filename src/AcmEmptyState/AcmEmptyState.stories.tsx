ce1bc65e9508ea1dbf6126b681f5e7f049691bc3
randybrunopiverger <rbrunopi@redhat.com>
2021-03-28 15:32:05 -0400

    prettier

--- src/AcmEmptyState/AcmEmptyState.stories.tsx
+++ src/AcmEmptyState/AcmEmptyState.stories.tsx
@@ -13,7 +13,11 @@ export default {
 export const EmptyState = () => (
     <AcmEmptyState
         title="No items found"
-        message={<React.Fragment><p>You don't have any items yet.</p> Click <strong>Create Item</strong> to create some.</React.Fragment>}
+        message={
+            <React.Fragment>
+                <p>You don't have any items yet.</p> Click <strong>Create Item</strong> to create some.
+            </React.Fragment>
+        }
         action={<Button variant="primary">Create Item</Button>}
     />
 )
