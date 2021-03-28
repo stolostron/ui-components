874b8857512e5d3dc6834d2f3faab50203253e26
randybrunopiverger <rbrunopi@redhat.com>
2021-03-28 15:42:55 -0400

    linter

--- src/AcmEmptyState/AcmEmptyState.stories.tsx
+++ src/AcmEmptyState/AcmEmptyState.stories.tsx
@@ -15,7 +15,7 @@ export const EmptyState = () => (
         title="No items found"
         message={
             <React.Fragment>
-                <p>You don't have any items yet.</p> Click <strong>Create Item</strong> to create some.
+                <p>You do not have any items.</p>Click <strong>Create Item</strong> to create some.
             </React.Fragment>
         }
         action={<Button variant="primary">Create Item</Button>}
