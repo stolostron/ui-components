e445728108894808e71afa04c3d8e05d7e14bc1e
Sho Weimer <sweimer@redhat.com>
2021-04-07 13:37:38 -0400

    fix story name

--- src/AcmTable/AcmTable.stories.tsx
+++ src/AcmTable/AcmTable.stories.tsx
@@ -66,7 +66,7 @@ export function TableStory(args: Record<string, unknown>) {
     )
 }
 
-export function TableStoryWithExpand(args: Record<string, unknown>) {
+export function TableWithExpand(args: Record<string, unknown>) {
     const [items, setItems] = useState<IExampleData[]>(exampleData.slice(0, 105))
     return (
         <div style={{ height: '100vh' }}>
