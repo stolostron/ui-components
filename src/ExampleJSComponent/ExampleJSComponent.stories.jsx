8ca30d8fbe59fd87de7a82941301380a0d31e5a2
Travis CI <travis@travis-ci.org>
2021-04-08 10:30:29 -0400

    Add index to each component

--- src/ExampleJSComponent/ExampleJSComponent.stories.jsx
+++
@@ -1,9 +0,0 @@
-// import React from 'react'
-// import ExampleJSComponent from './ExampleJSComponent'
-
-// export default {
-//     title: 'Example JS Component',
-//     component: ExampleJSComponent,
-// }
-
-// export const Example = () => <ExampleJSComponent text="Example JS Component" />
