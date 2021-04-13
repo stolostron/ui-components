f3f3948b9a7bd3b94dc86e5a208c9a1cca655ed1
Richie Piya <rpiyasirisilp@gmail.com>
2021-04-13 16:50:53 -0400

    updated to use expandable card

--- src/AcmExpandable/AcmExpandableCard/AcmExpandableCard.tsx
+++ src/AcmExpandable/AcmExpandableCard/AcmExpandableCard.tsx
@@ -1,28 +1,20 @@
 /* Copyright Contributors to the Open Cluster Management project */
 
 import React, { useState } from 'react'
-import { Accordion, AccordionItem, AccordionContent, AccordionToggle } from '@patternfly/react-core'
+import { Card, CardBody, CardExpandableContent, CardHeader, CardTitle } from '@patternfly/react-core'
 import { makeStyles } from '@material-ui/styles'
 
 const useStyles = makeStyles({
-    accordion: {
-        '--pf-c-accordion__toggle--m-expanded--before--BackgroundColor': 'none',
-        '--pf-c-accordion__toggle--hover__toggle-text--Color': 'none',
-        '--pf-c-accordion__toggle--active__toggle-text--Color': 'none',
-        '--pf-c-accordion__toggle--focus__toggle-text--Color': 'none',
-        '--pf-c-accordion__toggle--m-expanded__toggle-text--Color': 'none',
-        '--pf-c-accordion__expanded-content--m-expanded__expanded-content-body--before--BackgroundColor': 'none',
-        '--pf-c-accordion__toggle--hover--BackgroundColor': 'white',
-        '--pf-c-accordion__toggle--focus--BackgroundColor': 'white',
-        '--pf-c-accordion__toggle--active--BackgroundColor': 'white',
-        'box-shadow': '0 0.0625rem 0.125rem 0 rgba(3, 3, 3, 0.12), 0 0 0.125rem 0 rgba(3, 3, 3, 0.06)',
+    header: { 'border-bottom': '1px solid rgba(0,0,0,0.1)', cursor: 'pointer' },
+    headerExpanded: {
+        '& button': {
+            transform: 'rotate(90deg)',
+        },
     },
-    accordionHeader: { height: '64px', padding: '24px', 'border-bottom': '1px solid rgba(0,0,0,0.1)' },
-    accordionBody: {
+    body: {
         padding: '32px 24px',
         '& div': {
             padding: '0px',
-            color: 'black',
         },
     },
 })
@@ -36,20 +28,22 @@ export function AcmExpandableCard(props: {
     const [open, setOpen] = useState<boolean>(true)
     const classes = useStyles()
     return (
-        <Accordion className={`${classes.accordion} ${props.className}`} id={props.id}>
-            <AccordionItem>
-                <AccordionToggle
-                    className={classes.accordionHeader}
-                    id="details-card-header"
-                    isExpanded={open}
-                    onClick={() => setOpen(!open)}
-                >
-                    {props.title}
-                </AccordionToggle>
-                <AccordionContent isHidden={!open} className={classes.accordionBody}>
-                    {props.children}
-                </AccordionContent>
-            </AccordionItem>
-        </Accordion>
+        <Card id={props.id} className={props.className} isExpanded={open}>
+            <CardHeader
+                className={`${classes.header} ${open ? classes.headerExpanded : ''}`}
+                onExpand={() => setOpen(!open)}
+                onClick={() => setOpen(!open)}
+                toggleButtonProps={{
+                    id: 'toggle-button',
+                    'aria-label': 'Toggle details',
+                    'aria-expanded': open,
+                }}
+            >
+                <CardTitle>{props.title}</CardTitle>
+            </CardHeader>
+            <CardExpandableContent className={classes.body}>
+                <CardBody>{props.children}</CardBody>
+            </CardExpandableContent>
+        </Card>
     )
 }
