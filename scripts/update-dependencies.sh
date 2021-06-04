caeea21fb90b3ad4baedaf49bf873e48174ffe26
James Talton <jtalton@redhat.com>
2021-06-04 10:43:13 -0400

    fix-update-script (#362)
    Signed-off-by: James Talton <jtalton@redhat.com>

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -20,7 +20,7 @@ rm -rf node_modules
 rm -f package-lock.json
 
 npx npm-check-updates --doctor --upgrade --filter ts-loader --target minor
-npx npm-check-updates --doctor --upgrade --reject ts-loader typescript
+npx npm-check-updates --doctor --upgrade --reject ts-loader,typescript
 npm dedup
 npm audit fix
 
