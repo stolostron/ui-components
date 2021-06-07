351973f4e16e9af575b8f5c734212974bc87f527
Sho Weimer <sweimer@redhat.com>
2021-06-07 11:04:10 -0400

    AcmInlineCopy - add displayText prop and make whole text clickable
    Signed-off-by: Sho Weimer <sweimer@redhat.com>

--- src/AcmInlineCopy/AcmInlineCopy.test.tsx
+++ src/AcmInlineCopy/AcmInlineCopy.test.tsx
@@ -19,4 +19,17 @@ describe('AcmInlineCopy', () => {
             await new Promise((resolve) => setTimeout(resolve, 0))
         })
     })
+    test('renders with display text', async () => {
+        const { getByText, getByTestId, container } = render(
+            <AcmInlineCopy displayText="Non-copy text" text="copy text" id="copy" />
+        )
+        expect(getByTestId('copy')).toBeInTheDocument()
+        expect(getByText('Non-copy text')).toBeInTheDocument()
+        await act(async () => {
+            expect(await axe(container)).toHaveNoViolations()
+            await userEvent.click(getByTestId('copy'))
+            await expect(document.execCommand).toHaveBeenCalled()
+            await new Promise((resolve) => setTimeout(resolve, 0))
+        })
+    })
 })
