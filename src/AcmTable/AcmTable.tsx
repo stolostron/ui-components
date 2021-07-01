5aca39db763df7d569098ddac9a36e9b01dd7459
James Talton <jtalton@redhat.com>
2021-07-01 10:29:34 -0400

    Fix table sorting when selectable items with no bulk actions
    Signed-off-by: James Talton <jtalton@redhat.com>

--- src/AcmTable/AcmTable.tsx
+++ src/AcmTable/AcmTable.tsx
@@ -537,7 +537,7 @@ export function AcmTable<T>(props: AcmTableProps<T>) {
     // Compensate for PF auto-added columns
     // sort state always contains the data index
     // adjustedSort and the updateSort callback compensate for header display index used in PF
-    const adjustedSortIndexOffset = (bulkActions && bulkActions.length ? 1 : 0) + (onCollapse ? 1 : 0)
+    const adjustedSortIndexOffset = (bulkActions?.length || !!props.onSelect ? 1 : 0) + (onCollapse ? 1 : 0)
     const adjustedSort =
         sort && sort.index !== undefined && sort.index !== null && sort.direction && filtered.length > 0
             ? {
